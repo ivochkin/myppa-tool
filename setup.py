f0f3543f088a2b22a72ce4e15cb65bb4735c9060
Stanislav Ivochkin <isn@extrn.org>
2016-11-19 13:47:22 +0300

    Generate myppa/_version.py, add install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="myppa",
     setup_requires=["vcversioner"],
+    vcversioner={
+        'version_module_paths': ['myppa/_version.py'],
+    },
     author="Stanislav Ivochkin",
     author_email="isn@extrn.org",
     description=("Manage your own PPA"),
@@ -15,5 +18,10 @@ setup(
         "console_scripts": [
             "myppa = myppa.cli:main"
         ]
-    }
+    },
+    install_requires=[
+        "click>=6.0",
+        "Jinja2>=2.7",
+        "PyYAML>=3.12",
+    ],
 )
