405749c536f5a35b1c2b57cab48b02b3467b3369
Stanislav Ivochkin <isn@extrn.org>
2016-11-19 16:36:32 +0300

    Ignore non-empty directories for data.tar.gz

--- myppa/data/templates/build_deb.sh
+++ myppa/data/templates/build_deb.sh
@@ -53,8 +53,13 @@ EOT
 # myppa-pack-deb.sh
 cat <<EOT >myppa-pack-deb.sh
 #!/usr/bin/env bash
-<myppa-new-files xargs tar czf data.tar.gz
-<myppa-new-files xargs md5sum > md5sums
+for i in \$(cat myppa-new-files); do
+  if ! grep -q "^\$i/" myppa-new-files; then
+    echo \$i >> myppa-new-files-no-dirs
+  fi
+done
+<myppa-new-files-no-dirs xargs tar czf data.tar.gz
+<myppa-new-files-no-dirs xargs md5sum > md5sums
 fakeroot tar czvf control.tar.gz control changelog copyright md5sums
 fakeroot ar cr {{name}}_${version}_{{architecture}}.deb debian-binary control.tar.gz data.tar.gz
 EOT
