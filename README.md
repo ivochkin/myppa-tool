914a80ef1714f3881cbc1300d1345a096691548e
Stanislav Ivochkin <isn@extrn.org>
2016-11-23 19:22:37 +0300

    build_deb.sh facelifting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # myppa-tool
 My Personal Package Archive managing tool
+
+## Special variables you can refer to in specs:
+* $task\_id
+* $distribution
+* $codename
+* $architecture
+* $version
+* $git\_revision
--- myppa/data/templates/build_deb.sh
+++ myppa/data/templates/build_deb.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # Tree structure of the cwd:
 
-taskid=$(uuidgen)
+task_id=$(uuidgen)
+distribution="{{distribution}}"
+codename="{{codename}}"
+architecture="{{architecture}}"
+version="{{version}}"
+git_revision="{{git_revision}}"
 
 {% if http_proxy %}
 export http_proxy={{http_proxy}}
@@ -9,7 +14,7 @@ export https_proxy={{http_proxy}}
 
 {% endif %}
 git clone --recursive --branch={{git_revision}} {{git_repository}} src
-version={{version}}
+
 {% if version_script %}
 cd src
 version=$({{version_script}})
@@ -18,7 +23,9 @@ cd -
 {% if version_append_codename %}
 version=${version}~{{codename}}
 {% endif %}
+{% if version_append_codename or version_script %}
 
+{% endif %}
 # myppa-install-prerequisite.sh
 cat <<EOT > myppa-install-prerequisite.sh
 {% if http_proxy %}
@@ -28,7 +35,7 @@ export https_proxy={{http_proxy}}
 #!/usr/bin/env bash
 {% for package in build_depends %}
 apt-get install -y {{package}}
-{%- endfor %}
+{% endfor %}
 EOT
 
 # myppa-install-project.sh
@@ -38,8 +45,10 @@ cat <<EOT > myppa-install-project.sh
 export http_proxy={{http_proxy}}
 export https_proxy={{http_proxy}}
 {% endif %}
+{% if before_configure %}
 cd /myppa/src
 {{before_configure}}
+{% endif %}
 mkdir /myppa/build
 cd /myppa/build
 {% if builder == "cmake" %}
@@ -47,34 +56,40 @@ cmake {% for k, v in cmake_args.items() %}-D{{k}}={{v}} {% endfor %}../src
 {% elif builder == "autotools" %}
 ../src/configure {% for k, v in (configure_args or {}).items() %}--{{k}}={{v}} {% endfor %}
 {% endif %}
+{% if after_configure %}
 cd /myppa/src
 {{after_configure}}
+{% endif %}
+{% if before_compile %}
 cd /myppa/src
 {{before_compile}}
+{% endif %}
 cd /myppa/build
 make
+{% if after_compile %}
 cd /myppa/src
 {{after_compile}}
+{% endif %}
+{% if before_install %}
 cd /myppa/src
 {{before_install}}
+{% endif %}
 cd /myppa/build
 make install
+{% if after_install %}
 cd /myppa/src
 {{after_install}}
+{% endif %}
 EOT
 
 # myppa-pack-deb.sh
 cat <<EOT >myppa-pack-deb.sh
 #!/usr/bin/env bash
-for i in \$(cat myppa-new-files-raw); do
-  if ! grep -q "^\$i/" myppa-new-files-raw; then
-    echo \$i >> myppa-new-files-no-dirs
-  fi
-done
-cat myppa-new-files-no-dirs | egrep '({{install_paths | join(")|(")}})' > myppa-new-files
-<myppa-new-files xargs tar czf data.tar.gz
+cat myppa-new-files-raw | egrep '({{install_paths | join(")|(")}})' > myppa-new-files
+cat myppa-new-files
+<myppa-new-files xargs tar --create --gzip --no-recursion --file data.tar.gz
 <myppa-new-files xargs md5sum > md5sums
-fakeroot tar czvf control.tar.gz control changelog copyright md5sums
+fakeroot tar --create --gzip --file control.tar.gz control changelog copyright md5sums
 fakeroot ar cr {{name}}_${version}_{{architecture}}.deb debian-binary control.tar.gz data.tar.gz
 EOT
 
@@ -118,11 +133,11 @@ baseimage=ivochkin/myppa:{{distribution}}.{{codename}}.{{architecture}}
 docker pull $baseimage
 docker run --volume $(pwd):/myppa -w /myppa --cidfile stage1.cid $baseimage bash myppa-install-prerequisite.sh
 stage1cid=$(cat stage1.cid)
-stage1iid=myppa:$taskid-1
+stage1iid=myppa:${task_id}-1
 docker commit $stage1cid $stage1iid
 docker run --volume $(pwd):/myppa -w /myppa --cidfile stage2.cid $stage1iid bash myppa-install-project.sh
 stage2cid=$(cat stage2.cid)
-stage2iid=myppa:$taskid-2
+stage2iid=myppa:${task_id}-2
 docker diff $stage2cid | grep -e '^A' | awk '{print $2}' > myppa-new-files-raw
 docker commit $stage2cid $stage2iid
 docker run --rm --volume $(pwd):/myppa -w /myppa $stage2iid bash myppa-pack-deb.sh
